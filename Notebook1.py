972eb20b18d9b3fa73a70c31bd88ee93965f65f6
Sasa Bjeletic <69247397+Sasabj20@users.noreply.github.com>
2023-05-11 19:08:14 +0200

    Add files via upload

---
+++ Notebook1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import statsmodels.formula.api as sm
+
+df = pd.read_excel('E:Tabela29II.xlsx')
+rege = sm.ols(formula="Salary ~ Bonuses + Contribution", data=df).fit()
+print(df)
+X = df[["Bonuses","Contribution"]]
+y = df["Salary"]
+from sklearn.linear_model import LinearRegression
+# Note the difference in argument order
+#print(X)
+#print(y)
+
+#predictions = model.predict(X) # make the predictions by the model
+
+# Print out the statistics
+#print(rege.summary())
+
+#rege.fit = (X, y)
+#rege = LinearRegression().fit(X, y)
+#print(rege.coef_)
+#print(rege.intercept_)
+#print(rege.predict(X))
+#lr = LinearRegression()
+#x, y = data[:, :-1], data[:, -1]
+#lr.fit(X, y)
+#lr = LinearRegression(copy_X=True, fit_intercept=True, normalize=False)
+#print(lr.coef_)
+
+#print (model.coef_)
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.8, random_state=42)
+
+model = LinearRegression().fit(X_train, y_train)
+model.fit = (X_train, y_train)
+print(model.coef_)
+#score = model.score(x_train, y_train)
+
+#scores.append(score)
+#print(scores)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
